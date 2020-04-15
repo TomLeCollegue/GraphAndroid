23d20208dbc49032e3d0cdbe65e07e8a5b037962
Kubasik <tomkubasik.pro@gmail.com>
2020-04-15 16:40:29 +0200

    fix parcours noeuds

--- app/src/main/java/com/example/music_graph/InfoNode.java
+++ app/src/main/java/com/example/music_graph/InfoNode.java
@@ -39,10 +39,14 @@ public class InfoNode extends AppCompatActivity implements Adapter_Edges.OnItemC
     }
 
     public void onItemClick(int position) {
-        int id = MainActivity.graph.getNodes().get(0).getNeighbours().get(position).getEnd().getNodeId();
-        Intent intent = new Intent(InfoNode.this, InfoNode.class);
-        intent.putExtra("noeud", id);
-        finish();
-        startActivity(intent);
+        int id = MainActivity.graph.getNodes().get(positionNode).getNeighbours().get(position).getEnd().getNodeId();
+
+        positionNode = id;
+        nodesVoisins = MainActivity.graph.getNodes().get(positionNode).getNeighbours();
+        NameNode.setText(MainActivity.graph.getNodes().get(positionNode).getName());
+        rvInfo.setLayoutManager(new LinearLayoutManager(InfoNode.this, LinearLayoutManager.VERTICAL, false));
+        Adapter_Edges MyAdapter = new Adapter_Edges(nodesVoisins);
+        rvInfo.setAdapter(MyAdapter);
+        MyAdapter.setonItemClickListener(InfoNode.this);
     }
 }
